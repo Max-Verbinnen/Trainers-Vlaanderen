76cc140d762bbb96cff443309114b5c34a9ba08d
Max <max.verbinnen@skynet.be>
2021-07-07 19:01:53 +0200

    splitted routing for performance

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,7 @@
-import Home from "./pages/Home.vue"
-import AddTraining from "./pages/AddTraining.vue"
-import Videos from "./pages/Videos.vue"
-import Boards from "./pages/Boards.vue"
-import SingleTraining from "./pages/SingleTraining.vue"
-
 export default [
-  {path: "/", component: Home},
-  {path: "/toevoegen", component: AddTraining},
-  {path: "/videos", component: Videos},
-  {path: "/boards", component: Boards},
-  {path: "/training/:id", component: SingleTraining}
+  {path: "/", component: () => import("./pages/Home.vue")},
+  {path: "/toevoegen", component: () => import("./pages/AddTraining.vue")},
+  {path: "/videos", component: () => import("./pages/Videos.vue")},
+  {path: "/boards", component: () => import("./pages/Boards.vue")},
+  {path: "/training/:id", component: () => import("./pages/SingleTraining.vue")}
 ]
