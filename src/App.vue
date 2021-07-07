84bf603b6f4127048c9b36aac2fe33eb3c4a28a1
Max <max.verbinnen@skynet.be>
2021-07-07 13:52:44 +0200

    footer

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
   <div>
     <Header />
     <router-view></router-view>
+    <Footer />
     <BackToTopArrow />
   </div>
 </template>
 
 <script>
 import Header from "./components/Header.vue"
+import Footer from "./components/Footer.vue"
 import BackToTopArrow from "./components/BackToTopArrow.vue"
 
 export default {
   components: {
     Header,
+    Footer,
     BackToTopArrow
   }
 }
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,7 @@
 <template>
   <footer>
-    <p>&copy; Copyright Trainers Vlaanderen <span>{{new Date().getFullYear()}}</span></p>
+    <p class="copyright">&copy; Copyright Trainers Vlaanderen <span>{{new Date().getFullYear()}}</span></p>
+    <p class="collab">Tijl Verbesselt 🤝 <a href="https://www.maxverbinnen.be/" target="_blank" rel="noopener">Max Verbinnen</a></p>
   </footer>
 </template>
 
@@ -15,9 +16,21 @@
 footer {
   width: 100%;
   height: 4rem;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+  text-align: center;
+}
+
+.copyright {
+  background: rgb(250, 250, 250);
+  padding: 0.5rem 0;
+}
+
+.collab {
+  padding: 1rem 0;
+  background: rgb(246, 253, 243);
+}
+
+footer a {
+  color: black;
 }
 
 </style>
