721b37cf7569b10c68a704bffc00528314f35c99
Max Verbinnen <max.verbinnen@skynet.be>
2021-08-21 13:53:38 +0200

    env

--- src/firebase.js
+++ src/firebase.js
@@ -4,15 +4,16 @@ import 'firebase/analytics'
 import 'firebase/auth'
 import 'firebase/storage'
 
+// This info may be public, as firebase rules serve as protection
 const firebaseConfig = {
   apiKey: process.env.VUE_APP_API_KEY,
-  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
-  databaseURL: process.env.VUE_APP_DATABASE_URL,
-  projectId: process.env.VUE_APP_PROJECT_ID,
-  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
-  appId: process.env.VUE_APP_APP_ID,
-  measurementId: process.env.VUE_APP_MEASUREMENT_ID
+  authDomain: "trainers-vlaanderen-51280.firebaseapp.com",
+  databaseURL: "https://trainers-vlaanderen-51280-default-rtdb.firebaseio.com/",
+  projectId: "trainers-vlaanderen-51280",
+  storageBucket: "trainers-vlaanderen-51280.appspot.com",
+  messagingSenderId: "45370909626",
+  appId: "1:45370909626:web:cb12d14819cd4d28587383",
+  measurementId: "G-GX1MLSS2R8"
 };
 
 firebase.initializeApp(firebaseConfig);
