e73a158fb27cb1e216c75228ca2739a396a8ce06
Max Verbinnen <max.verbinnen@skynet.be>
2021-08-21 14:14:31 +0200

    fixed copying err

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,13 @@ import 'firebase/storage'
 // This info may be public, as firebase rules serve as protection
 const firebaseConfig = {
   apiKey: process.env.VUE_APP_API_KEY,
-  authDomain: process.env.VUE_APP_API_KEY,
-  databaseURL: process.env.VUE_APP_API_KEY,
-  projectId: process.env.VUE_APP_API_KEY,
-  storageBucket: process.env.VUE_APP_API_KEY,
-  messagingSenderId: process.env.VUE_APP_API_KEY,
-  appId: process.env.VUE_APP_API_KEY,
-  measurementId: process.env.VUE_APP_API_KEY
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_DATABASE_URL,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_APP_ID,
+  measurementId: process.env.VUE_APP_MEASUREMENT_ID
 };
 
 firebase.initializeApp(firebaseConfig);
